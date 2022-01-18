3c1dee4fc1e0c38b9a6f6599bcd76b382e7785e8
Lucas van Pelt <lucasvpelt03@gmail.com>
2022-01-18 11:33:41 +0100

    Random spel kiezer

--- spel.py
+++ spel.py
@@ -1,13 +1,37 @@
-import random
+import random, time, os
 global spelList
+
+def clearScreen(sleepTime):
+    time.sleep(sleepTime)
+    os.system("cls")
 
 #           0           1          2         3        4         5                     6
 spelList = ['Monopoly', 'Yathzee', 'Bridge', 'Poker', 'Pesten', 'Mens erger je niet', 'Cluedo']
 
-def spelProgramma(spelList):
+def spelProgramma7(spelList):
     result = []
     for i in range (len(spelList)):
         result.append(random.choice(spelList))
     return result
 
-print(spelProgramma(spelList))
+def spelProgramma3(spelList):
+    result = []
+    for i in range (3):
+        result.append(random.choice(spelList))
+    return result
+
+def spelProgramma10(spelList):
+    result = []
+    for i in range (10):
+        result.append(random.choice(spelList))
+    return result
+
+print("3 spelen: ")
+print(*spelProgramma3(spelList),sep='\n')
+clearScreen(5)
+print("7 spelen: ")
+print(*spelProgramma7(spelList),sep='\n')
+clearScreen(5)
+print("10 spelen: ")
+print(*spelProgramma10(spelList),sep='\n')
+clearScreen(5)
